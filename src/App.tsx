a67169bf736c7858b7ebd5241979a2a1d18a3e6b
Azizbek <109634497+Aziz706@users.noreply.github.com>
2023-05-09 20:51:09 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Page1 from "./Components/Page1/Page1";
 import Page2 from "./Components/Page2/Page2";
 import Page2id from "./Components/Page2/Page2id";
+import { NavLink } from 'react-router-dom';
 
 const api = {
   key: "adb3e547b13e79746fac1a5d9c5ca410",
@@ -13,9 +14,10 @@ function App() {
   return (
     <div>
       <div>
+        <NavLink to={"/"}>Go to home</NavLink>
       <div style={{width: "100%", height: "100%"}} >
         <Routes>
-        <Route path='/Weather-app/' element={<Page1 api={api} />} />
+        <Route path='/' element={<Page1 api={api} />} />
         <Route path='/page-1' element={<Page1 api={api} />} />
         <Route path='/page-2' element={<Page2 api={api} />} />
         <Route path='/page-2/:id' element={<Page2id api={api} />} />
